6d14ac963696e1271b74cfd025432c01eae4e80c
shunf4 <shun1048576@gmail.com>
2022-07-20 19:45:18 +0800

    feat: change network profile category (public/private/domain)

--- MainForm.cs
+++ MainForm.cs
@@ -24,7 +24,7 @@ namespace NetworkRenameTool {
             using (var profilesKey = Registry.LocalMachine.OpenSubKey(ProfilesPath)) {
                 foreach (var subkeyName in profilesKey.GetSubKeyNames()) {
                     using (var subkey = profilesKey.OpenSubKey(subkeyName)) {
-                        NetworksGrid.Rows.Add(new[] { subkeyName, subkey.GetValue(ProfileNameKey), subkey.GetValue(DescriptionKey) });
+                        NetworksGrid.Rows.Add(new[] { subkeyName, subkey.GetValue(ProfileNameKey), subkey.GetValue(DescriptionKey), System.Convert.ToInt32((subkey.GetValue(CategoryKey))) });
                     }
                 }
             }
@@ -42,6 +42,8 @@ namespace NetworkRenameTool {
                 var subkeyName = (string)row.Cells[0].Value;
                 var targetKeyName = $"{ProfilesPath}\\{subkeyName}";
                 string value;
+                int valueInt32;
+                bool int32ParseSuccess;
                 switch (e.ColumnIndex) {
                     case 1:
                         value = (string)row.Cells[1].Value;
@@ -50,6 +52,16 @@ namespace NetworkRenameTool {
                     case 2:
                         value = (string)row.Cells[2].Value;
                         using (var subkey = Registry.LocalMachine.OpenSubKey(targetKeyName, true)) subkey.SetValue(DescriptionKey, value);
+                        break;
+                    case 3:
+                        value = (string)row.Cells[3].Value;
+                        int32ParseSuccess = System.Int32.TryParse(value, out valueInt32);
+                        if (!int32ParseSuccess)
+                        {
+                            valueInt32 = 0;
+                        }
+                        using (var subkey = Registry.LocalMachine.OpenSubKey(targetKeyName, true)) subkey.SetValue(CategoryKey, valueInt32);
+                        row.Cells[3].Value = valueInt32.ToString();
                         break;
                 }
             }
@@ -79,6 +91,7 @@ namespace NetworkRenameTool {
         private const string ProfilesPath = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles";
         private const string ProfileNameKey = "ProfileName";
         private const string DescriptionKey = "Description";
+        private const string CategoryKey = "Category";
 
         #endregion
 
